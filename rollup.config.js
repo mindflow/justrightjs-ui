f3ed2ccb396a39b6c7511411eac0fe62ca3e7f64
Mindflow <torbjorn.nilsen@gmail.com>
2021-01-02 01:23:17 +1100

    Min.

--- rollup.config.js
+++ rollup.config.js
@@ -16,8 +16,22 @@ export default [{
         multiEntry(),
         postprocess([
             [/(?<=import\s*(.*)\s*from\s*)['"]((?!.*[.]js).*)['"];/, '\'./$2.js\'']
+        ])
+    ]
+},{
+    input: "src/**/*.js",
+    external: [ 'coreutil_v1','xmlparser_v1','mindi_v1','justright_core_v1' ],
+    output: {
+        name: 'justright_ui_v1',
+        file: "dist/jsm/justright_ui_v1.min.js",
+        format: "es"
+    },
+    plugins: [
+        multiEntry(),
+        postprocess([
+            [/(?<=import\s*(.*)\s*from\s*)['"]((?!.*[.]js).*)['"];/, '\'./$2.js\'']
         ]),
-        //uglify()
+        uglify()
     ]
 },{
     input: "src/**/*.js",
