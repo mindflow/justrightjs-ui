c7d6d9930fb524c81a9c6cd5b753b811f18d022d
Toby <torbjorn.nilsen@gmail.com>
2022-04-27 10:29:28 +1000

    Added Panel and Background.

---
+++ src/justright/component/background/background.js
@@ -0,0 +1,30 @@
+import { Component, ComponentFactory, CanvasStyles } from "justright_core_v1";
+import { Logger } from "coreutil_v1";
+import { InjectionPoint } from "mindi_v1";
+
+const LOG = new Logger("Background");
+
+export class Background {
+
+	static get COMPONENT_NAME() { return "Background"; }
+	static get TEMPLATE_URL() { return "/assets/justrightjs-ui/background.html"; }
+	static get STYLES_URL() { return "/assets/justrightjs-ui/background.css"; }
+
+    constructor(){
+		/** @type {ComponentFactory} */
+		this.componentFactory = InjectionPoint.instance(ComponentFactory);
+
+		/** @type {Component} */
+		this.component = null;
+	}
+
+	set(key,val) {
+		this.component.set(key,val);
+	}
+
+	postConfig() {
+		this.component = this.componentFactory.create(Background.COMPONENT_NAME);
+		CanvasStyles.enableStyle(Background.COMPONENT_NAME);
+	}
+
+}
---
+++ src/justright/component/panel/panel.js
@@ -0,0 +1,66 @@
+import { ComponentFactory, Component, CanvasStyles } from "justright_core_v1";
+import { InjectionPoint } from "mindi_v1";
+import { Logger } from "coreutil_v1";
+
+const LOG = new Logger("Panel");
+
+export class Panel {
+
+	static get COMPONENT_NAME() { return "Panel"; }
+    static get TEMPLATE_URL() { return "/assets/justrightjs-ui/panel.html"; }
+    static get STYLES_URL() { return "/assets/justrightjs-ui/panel.css"; }
+
+    static get PARAMETER_STYLE_TYPE_COLUMN_ROOT() { return " panel-type-column-root "; }
+    static get PARAMETER_STYLE_TYPE_COLUMN() { return " panel-type-column "; }
+    static get PARAMETER_STYLE_TYPE_ROW() { return " panel-type-row "; }
+
+    static get PARAMETER_STYLE_CONTENT_ALIGN_LEFT() { return " panel-content-align-left "; }
+    static get PARAMETER_STYLE_CONTENT_ALIGN_RIGHT() { return " panel-content-align-right "; }
+    static get PARAMETER_STYLE_CONTENT_ALIGN_CENTER() { return " panel-content-align-center "; }
+    static get PARAMETER_STYLE_CONTENT_ALIGN_JUSTIFY() { return " panel-content-align-justify "; }
+
+    static get PARAMETER_STYLE_SIZE_AUTO() { return " panel-size-auto "; }
+    static get PARAMETER_STYLE_SIZE_MINIMAL() { return " panel-size-minimal "; }
+    static get PARAMETER_STYLE_SIZE_RESPONSIVE() { return " panel-size-responsive "; }
+
+    static get OPTION_STYLE_CONTENT_PADDING_SMALL() { return 1; }
+    static get OPTION_STYLE_CONTENT_PADDING_LARGE() { return 2; }
+
+    static get OPTION_STYLE_BORDER_SHADOW() { return 3; }
+
+    /**
+     * 
+     * @param {string} type 
+     * @param {string} contentAlign 
+     * @param {string} size 
+     * @param {Array<string>} options 
+     */
+    constructor(type = Panel.PARAMETER_STYLE_TYPE_COLUMN_ROOT,
+        contentAlign = Panel.PARAMETER_STYLE_CONTENT_ALIGN_CENTER,
+        size = Panel.PARAMETER_STYLE_SIZE_AUTO,
+        options = []) {
+
+        /** @type {ComponentFactory} */
+        this.componentFactory = InjectionPoint.instance(ComponentFactory);
+
+        /** @type {Component} */
+        this.component = null;
+
+        /** @type {string} */
+        this.type = type;
+
+        /** @type {string} */
+        this.contentAlign = contentAlign;
+
+        /** @type {string} */
+        this.size = size;
+
+    }
+
+    postConfig() {
+        this.component = this.componentFactory.create("Panel");
+        this.component.get("panel").setAttributeValue("class", this.type + this.contentAlign + this.size);
+        CanvasStyles.enableStyle(Panel.COMPONENT_NAME);
+    }
+
+}
