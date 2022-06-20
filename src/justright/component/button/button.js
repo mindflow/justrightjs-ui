840e01033007b46561f81f11c27be72c5ea81222
Toby <torbjorn.nilsen@gmail.com>
2022-06-21 02:03:48 +1000

    EventManager refactoring.

--- src/justright/component/button/button.js
+++ src/justright/component/button/button.js
@@ -1,36 +1,37 @@
 import {
     ComponentFactory,
     CanvasStyles,
-    Component
+    Component,
+    EventManager
 } from "justright_core_v1";
 import { InjectionPoint } from "mindi_v1";
 import { Logger, Method } from "coreutil_v1";
-import { CommonListeners } from "../commonListeners.js";
 
 const LOG = new Logger("Button");
 
 export class Button {
 
 	static get COMPONENT_NAME() { return "Button"; }
-    static get TEMPLATE_URL() { return "/assets/justrightjs-ui/button.html"; }
-    static get STYLES_URL() { return "/assets/justrightjs-ui/button.css"; }
+    static get TEMPLATE_URL()   { return "/assets/justrightjs-ui/button.html"; }
+    static get STYLES_URL()     { return "/assets/justrightjs-ui/button.css"; }
 
-    static get TYPE_PRIMARY() { return "button-primary"; }
+    static get TYPE_PRIMARY()   { return "button-primary"; }
     static get TYPE_SECONDARY() { return "button-secondary"; }
-    static get TYPE_SUCCESS() { return "button-success"; }
-    static get TYPE_INFO() { return "button-info"; }
-    static get TYPE_WARNING() { return "button-warning"; }
-    static get TYPE_DANGER() { return "button-danger"; }
-    static get TYPE_LIGHT() { return "button-light"; }
-    static get TYPE_DARK() { return "button-dark"; }
+    static get TYPE_SUCCESS()   { return "button-success"; }
+    static get TYPE_INFO()      { return "button-info"; }
+    static get TYPE_WARNING()   { return "button-warning"; }
+    static get TYPE_DANGER()    { return "button-danger"; }
+    static get TYPE_LIGHT()     { return "button-light"; }
+    static get TYPE_DARK()      { return "button-dark"; }
+
+    static get EVENT_CLICKED()      { return "click"; }
 
     /**
      * 
      * @param {string} label
-     * @param {CommonListeners} commonListeners
      * @param {string} buttonType
      */
-    constructor(label, commonListeners = null, buttonType = Button.TYPE_PRIMARY) {
+    constructor(label, buttonType = Button.TYPE_PRIMARY) {
 
         /** @type {ComponentFactory} */
         this.componentFactory = InjectionPoint.instance(ComponentFactory);
@@ -41,23 +42,33 @@ export class Button {
         /** @type {string} */
         this.label = label;
 
-        /** @type {CommonListeners} */
-        this.commonListeners = (null != commonListeners) ? commonListeners : new CommonListeners();
-
         /** @type {string} */
         this.buttonType = buttonType;
+
+        /** @type {EventManager<Button>} */
+        this.eventManager = new EventManager();
     }
+
+    /** @type {EventManager<Button>} */
+    get events() { return this.eventManager; }
 
     postConfig() {
         this.component = this.componentFactory.create("Button");
         CanvasStyles.enableStyle(Button.COMPONENT_NAME);
         this.component.get("button").setChild(this.label);
         this.component.get("button").setAttributeValue("class","button " + this.buttonType);
-        this.component.get("button").listenTo("click", new Method(this, this.clicked));
+        this.component.get("button").listenTo("click", new Method(this, (event) => {
+            this.eventManager.trigger(Button.EVENT_CLICKED, event);
+        }));
     }
 
-    clicked(event) {
-        this.commonListeners.callClick(event);
+    /**
+     * 
+     * @param {Method} method 
+     */
+    withClickListener(method) {
+        this.component.get("button").listenTo("click", method);
+        return this;
     }
 
     enableLoading() {
