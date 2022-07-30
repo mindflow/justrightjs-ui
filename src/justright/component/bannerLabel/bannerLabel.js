39c0b5fbbff967babe84490482d7792ce9fb4f4b
Toby <torbjorn.nilsen@gmail.com>
2022-07-31 02:33:33 +1000

    Introduce bannerlabel.

---
+++ src/justright/component/bannerLabel/bannerLabel.js
@@ -0,0 +1,107 @@
+import { Method } from "coreutil_v1";
+import { CanvasStyles, ComponentFactory } from "justright_core_v1";
+import { InjectionPoint } from "mindi_v1";
+import { CustomAppearance } from "../customAppearance.js";
+import { BannerLabelMessage } from "./bannerLabelMessage/bannerLabelMessage.js";
+
+export class BannerLabel {
+
+	static get COMPONENT_NAME() { return "BannerLabel"; }
+    static get TEMPLATE_URL() { return "/assets/justrightjs-ui/bannerLabel.html"; }
+    static get STYLES_URL() { return "/assets/justrightjs-ui/bannerLabel.css"; }
+
+    constructor() {
+        /** @type {ComponentFactory} */
+        this.componentFactory = InjectionPoint.instance(ComponentFactory);
+
+        /** @type {Component} */
+        this.component = null;
+
+		this.appearance = new CustomAppearance()
+			.withSize(CustomAppearance.SIZE_SMALL)
+			.withShape(CustomAppearance.SHAPE_ROUND)
+			.withSpacing(CustomAppearance.SPACING_BELOW);
+
+		/** @type {BannerLabelMessage} */
+		this.success = InjectionPoint
+			.instance(BannerLabelMessage, ["", BannerLabelMessage.TYPE_SUCCESS, this.appearance]);
+
+		/** @type {BannerLabelMessage} */
+		this.warning = InjectionPoint
+			.instance(BannerLabelMessage, ["", BannerLabelMessage.TYPE_WARNING, this.appearance]);
+
+		/** @type {BannerLabelMessage} */
+		this.error = InjectionPoint
+			.instance(BannerLabelMessage, ["", BannerLabelMessage.TYPE_ALERT, this.appearance]);
+
+        this.isVisible = false;
+    }
+
+    async postConfig() {
+        this.component = this.componentFactory.create(BannerLabel.COMPONENT_NAME);
+        CanvasStyles.enableStyle(BannerLabel.COMPONENT_NAME);
+        this.success.hide();
+        this.warning.hide();
+        this.error.hide();
+        this.component.get("bannerLabel").addChild(this.success.component);
+        this.component.get("bannerLabel").addChild(this.warning.component);
+        this.component.get("bannerLabel").addChild(this.error.component);
+        this.success.eventManager.listenTo(BannerLabelMessage.EVENT_CLOSE_CLICKED, new Method(this, this.hide));
+        this.warning.eventManager.listenTo(BannerLabelMessage.EVENT_CLOSE_CLICKED, new Method(this, this.hide));
+        this.error.eventManager.listenTo(BannerLabelMessage.EVENT_CLOSE_CLICKED, new Method(this, this.hide));
+        this.active = this.success;
+    }
+
+    /**
+     * 
+     * @param {String} header 
+     * @param {String} message 
+     */
+    showSuccess(header, message) {
+        this.showBanner(this.success, header, message);
+    }
+
+    /**
+     * 
+     * @param {String} header 
+     * @param {String} message 
+     */
+    showWarning(header, message) {
+        this.showBanner(this.warning, header, message);
+    }
+
+    /**
+     * 
+     * @param {String} header 
+     * @param {String} message 
+     */
+    showError(header, message) {
+        this.showBanner(this.error, header, message);
+    }
+
+    /**
+     * 
+     * @param {String} header 
+     * @param {String} message 
+     */
+    hide() {
+		this.component.get("bannerLabel").setAttributeValue("class", "banner-label banner-label-hidden");
+        this.active.hide();
+        this.isVisible = false;
+    }
+
+    /**
+     * 
+     * @param {BannerLabelMessage} banner
+     * @param {String} header
+     * @param {String} message
+     */
+     showBanner(banner, header, message) {
+        this.hide();
+		banner.setMessage(header, message);
+        banner.show();
+        this.component.get("bannerLabel").setAttributeValue("class", "banner-label banner-label-visible");
+        this.isVisible = true;
+		this.active = banner;
+    }
+}
---
+++ src/justright/component/bannerLabel/bannerLabelMessage/bannerLabelMessage.js
@@ -0,0 +1,115 @@
+import { TimePromise } from "coreutil_v1";
+import { CanvasStyles, ComponentFactory, EventManager } from "justright_core_v1";
+import { InjectionPoint } from "mindi_v1";
+import { CustomAppearance } from "../../customAppearance.js";
+
+export class BannerLabelMessage {
+
+	static get COMPONENT_NAME() { return "BannerLabelMessage"; }
+    static get TEMPLATE_URL() { return "/assets/justrightjs-ui/bannerLabelMessage.html"; }
+    static get STYLES_URL() { return "/assets/justrightjs-ui/bannerLabelMessage.css"; }
+
+    static get EVENT_CLOSE_CLICKED() { return "closeClicked"; }
+
+    static get TYPE_ALERT() { return "type-alert"; }
+    static get TYPE_INFO() { return "type-info"; }
+    static get TYPE_SUCCESS() { return "type-success"; }
+    static get TYPE_WARNING() { return "type-warning"; }
+
+    constructor(message, bannerType = BannerLabelMessage.TYPE_INFO, customAppearance = null) {
+
+        /** @type {ComponentFactory} */
+        this.componentFactory = InjectionPoint.instance(ComponentFactory);
+
+        /** @type {Component} */
+        this.component = null;
+
+        /** @type {String} */
+        this.header = null;
+
+        /** @type {String} */
+        this.message = message;
+
+        /** @type {string} */
+        this.bannerType = bannerType;
+
+        /** @type {CustomAppearance} */
+        this.customAppearance = customAppearance;
+
+        /** @type {EventManager} */
+        this.eventManager = new EventManager();
+    }
+
+    async postConfig() {
+        this.component = this.componentFactory.create(BannerLabelMessage.COMPONENT_NAME);
+        CanvasStyles.enableStyle(BannerLabelMessage.COMPONENT_NAME);
+        this.applyClasses("banner-label-message");
+        this.component.get("bannerLabelMessageCloseButton").listenTo("click", () => {
+            this.eventManager.trigger(BannerLabelMessage.EVENT_CLOSE_CLICKED);
+        });
+    }
+
+    hide() {
+        this.applyClasses("banner-label-message banner-label-message-hidden");
+        this.isVisible = false;
+        this.toggleDisplay();
+    }
+
+    toggleDisplay() {
+        TimePromise.asPromise(500, () => {
+            if (!this.isVisible) {
+                this.component.get("bannerLabelMessage").setAttributeValue("style", "display:none");
+            }
+        });
+    }
+
+    toggleVisibility() {
+        TimePromise.asPromise(50, () => {
+            if (this.isVisible) {
+                this.applyClasses("banner-label-message banner-label-message-visible");
+            }
+        });
+    }
+
+    show() {
+        this.component.get("bannerLabelMessage").setAttributeValue("style", "display:block");
+        this.toggleVisibility();
+        this.isVisible = true;
+    }
+
+    setMessage(header, message) {
+        if (header) {
+            this.applyHeader(header);
+        }
+        if (message) {
+            this.applyMessage(message);
+        }
+    }
+
+    applyHeader(header) {
+        this.header = header;
+        this.component.get("bannerLabelMessageHeader").setChild(this.header);
+    }
+
+    applyMessage(message) {
+        this.message = message;
+        this.component.get("bannerLabelMessageText").setChild(this.message);
+    }
+
+    applyClasses(baseClasses) {
+        let classes = baseClasses;
+        classes = classes + " banner-label-message-" + this.bannerType;
+        if (this.customAppearance) {
+            if (this.customAppearance.shape) {
+                classes = classes + " banner-label-message-" + this.customAppearance.shape;
+            }
+            if (this.customAppearance.size) {
+                classes = classes + " banner-label-message-" + this.customAppearance.size;
+            }
+            if (this.customAppearance.spacing) {
+                classes = classes + " banner-label-message-" + this.customAppearance.spacing;
+            }
+        }
+        this.component.get("bannerLabelMessageContent").setAttributeValue("class", classes);
+    }
+}
