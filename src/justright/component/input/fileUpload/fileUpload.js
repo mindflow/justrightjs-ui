de1db562b7b27256a208db60004530a6d436c649
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2025-10-04 22:04:00 +1000

    Extract FileUpload to justright ui,

---
+++ src/justright/component/input/fileUpload/fileUpload.js
@@ -0,0 +1,244 @@
+import { ContainerEvent, ContainerFileData } from "containerbridge_v1";
+import { Logger, Method } from "coreutil_v1";
+import { CanvasStyles, Component, ComponentFactory, EventManager, SimpleElement, CSS, HTML, StateManager } from "justright_core_v1";
+import { InjectionPoint, Provider } from "mindi_v1";
+import { FileUploadEntry } from "./fileUploadEntry/fileUploadEntry.js";
+import { CommonEvents } from "../../common/commonEvents.js";
+
+const LOG = new Logger("FileUpload");
+
+export class FileUpload {
+
+	static COMPONENT_NAME = "FileUpload";
+	static TEMPLATE_URL = "/assets/justrightjs-ui/fileUpload.html";
+	static STYLES_URL = "/assets/justrightjs-ui/fileUpload.css";
+
+	static DEFAULT_PLACEHOLDER = "FileUpload";
+
+	static EVENT_CLICKED = CommonEvents.CLICKED;
+    static EVENT_FILE_ADDED = "fileAdded";
+    static EVENT_FILE_REMOVED = "fileRemoved";
+
+    /**
+     * 
+     * @param {string} name 
+     * @param {boolean} multiple
+     * @param {Array<string>} fileTypeArray
+     */
+    constructor(name, multiple = false, fileTypeArray = []) {
+        
+        /** @type {ComponentFactory} */
+        this.componentFactory = InjectionPoint.instance(ComponentFactory);
+
+        /** @type {EventManager} */
+        this.events = new EventManager();
+
+        /** @type {Component} */
+        this.component = null;
+
+        /** @type {string} */
+        this.name = name;
+
+        /** @type {boolean} */
+        this.multiple = multiple;
+        
+        /** @type {string[]} */
+        this.fileTypeArray = fileTypeArray;
+
+        /** @type {StateManager<ContainerFileData>}  */
+        this.fileArrayState = new StateManager();
+
+        /** @type {Provider<FileUploadEntry>} */
+        this.fileUploadEntryProvider = InjectionPoint.provider(FileUploadEntry);
+
+    }
+
+    postConfig() {
+        this.component = this.componentFactory.create(FileUpload.COMPONENT_NAME);
+        CanvasStyles.enableStyle(FileUpload.COMPONENT_NAME);
+
+        
+        /** @type {SimpleElement} */
+        const uploadBox = this.component.get("uploadBox");
+        uploadBox.listenTo("dragover", new Method(this, this.dragOver));
+        uploadBox.listenTo("dragleave", new Method(this, this.dragLeave));
+        uploadBox.listenTo("drop", new Method(this, this.fileDropped));
+        uploadBox.listenTo("click", new Method(this, this.fileInputClicked));
+
+        if (this.multiple) {
+            const fileInput = this.component.get("fileInput");
+            fileInput.containerElement.setAttributeValue("multiple", "multiple");
+        }
+
+        const fileInput = this.component.get("fileInput");
+        fileInput.listenTo("change", new Method(this, this.fileInputChanged));
+
+    }
+
+    /**
+     * 
+     * @param {ContainerEvent} event 
+     */
+    fileInputClicked(event) {
+        const fileInput = this.component.get("fileInput");
+        fileInput.containerElement.value = null;
+        fileInput.containerElement.click();
+    }
+
+
+    /**
+     * @param {ContainerEvent} event
+     */
+    fileInputChanged(event) {
+        this.processFiles(event.files);
+    }
+
+    /**
+     * Process uploaded files and validate against file type array
+     * @param {ContainerFileData[]} files
+     */
+    async processFiles(files) {
+        const supportedFiles = [];
+        const unsupportedFiles = [];
+
+        for (const file of files) {
+            const supportedFile = this.isFileTypeSupported(file);
+            const fileAlreadySeleted = this.fileAlreadySeleted(file);
+            if (supportedFile && !fileAlreadySeleted) {
+                supportedFiles.push(file);
+            }
+            if (!supportedFile) {
+                unsupportedFiles.push(file);
+            }
+        }
+
+        // Handle supported files
+        if (supportedFiles.length > 0) {
+            if (this.multiple === false) {
+                this.fileArrayState.clear();
+            }
+            for (const file of supportedFiles) {
+                this.fileArrayState.update(file, file.name);
+                if (this.multiple === false) {
+                    break;
+                }
+            }
+        }
+
+        // Show unsupported files
+        this.showUnsupportedFiles(unsupportedFiles);
+        await this.updateFileList();
+        for (const file of this.fileArrayState.objectMap.values()) {
+            this.events.trigger(FileUpload.EVENT_FILE_ADDED, [file]);
+        }
+    }
+
+    fileAlreadySeleted(file) {
+        return this.fileArrayState.objectMap.has(file.name);
+    }
+
+    /**
+     * Check if file type is supported
+     * @param {ContainerFileData} file
+     * @returns {boolean}
+     */
+    isFileTypeSupported(file) {
+        // If fileTypeArray is empty, accept all files
+        if (this.fileTypeArray.length === 0) {
+            return true;
+        }
+
+        // Check if file's MIME type matches any in the fileTypeArray
+        return this.fileTypeArray.includes(file.type);
+    }
+
+    /**
+     * Display unsupported files in the unsupported div
+     * @param {Array<File>} unsupportedFiles
+     */
+    showUnsupportedFiles(unsupportedFiles) {
+        const unsupportedDiv = this.component.get("unsupported");
+        unsupportedDiv.clear();
+
+        if (unsupportedFiles.length > 0) {
+            unsupportedDiv.clear();
+            for (const file of unsupportedFiles) {
+                const messageElement = HTML.custom("div");
+                messageElement.setAttributeValue("class","file-upload-unsupported-file");
+                messageElement.setChild(`File "${file.name}" is not supported.`);
+                unsupportedDiv.addChild(messageElement);
+            }
+        }
+    }
+
+    /**
+     * @param {ContainerEvent} event
+     */
+    dragOver(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const uploadBox = this.component.get("uploadBox");
+        CSS.from(uploadBox).enable("file-upload-box-dragover");
+    }
+
+    /**
+     * @param {ContainerEvent} event
+     */
+    dragLeave(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const uploadBox = this.component.get("uploadBox");
+        CSS.from(uploadBox).disable("file-upload-box-dragover");
+    }
+
+    /**
+     *  @param {ContainerEvent} event
+     */
+    fileDropped(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const uploadBox = this.component.get("uploadBox");
+        CSS.from(uploadBox).disable("file-upload-box-dragover");
+
+        this.processFiles(event.files);
+    }
+
+    async updateFileList() {
+        const fileList = this.component.get("fileList");
+        fileList.clear();
+        
+        for (const file of this.fileArrayState.objectMap.values()) {
+            const fileEntry = await this.fileUploadEntryProvider.get([file]);
+            fileEntry.events.listenTo(FileUploadEntry.EVENT_REMOVE_CLICKED, new Method(this, this.removeFileEntry, [fileEntry]));
+            this.fileArrayState.reactTo(file.name, new Method(fileEntry, fileEntry.updateProgress));
+            fileList.addChild(fileEntry.component);
+        }
+    }
+
+    /**
+     * 
+     * @param {ContainerEvent} event
+     * @param {File} file
+     * @param {any} args
+     */
+    async removeFileEntry(event, file, args) {
+        this.fileArrayState.delete(file.name);
+        // Clear unsupported files when updating file list
+        const unsupportedDiv = this.component.get("unsupported");
+        unsupportedDiv.clear();
+        await this.updateFileList();
+        // Prevent the click event from bubbling up to the upload box
+        event.stopPropagation();
+    }
+
+    clicked(event) {
+        this.events.trigger(FileUpload.EVENT_CLICKED, [event]);
+    }
+
+    focus() {
+
+    }
+}
---
+++ src/justright/component/input/fileUpload/fileUploadEntry/fileUploadEntry.js
@@ -0,0 +1,80 @@
+import { ContainerEvent, ContainerFileData } from "containerbridge_v1";
+import { Method } from "coreutil_v1";
+import { CanvasStyles, Component, ComponentFactory, EventManager } from "justright_core_v1";
+import { InjectionPoint } from "mindi_v1";
+
+export class FileUploadEntry {
+    
+    static COMPONENT_NAME = "FileUploadEntry"
+    static TEMPLATE_URL = "/assets/justrightjs-ui/fileUploadEntry.html"
+    static STYLES_URL = "/assets/justrightjs-ui/fileUploadEntry.css"
+    
+    static EVENT_REMOVE_CLICKED = "removeClicked";
+
+    /**
+     * 
+     * @param {ContainerFileData} file 
+     */
+    constructor(file) {
+
+        /** @type {ComponentFactory} */
+        this.componentFactory = InjectionPoint.instance(ComponentFactory);
+        
+        /** @type {EventManager} */
+        this.events = new EventManager();
+        
+        /** @type {Component} */
+        this.component = null;
+        
+        /** @type {ContainerFileData} */
+        this.file = file;
+        
+        /** @type {string} */
+        this.fileName = file.name;
+        
+        /** @type {number} */
+        this.fileSize = file.size;
+        
+        /** @type {string} */
+        this.fileType = file.type;
+    }
+    
+    async postConfig() {
+        this.component = this.componentFactory.create(FileUploadEntry.COMPONENT_NAME);
+        CanvasStyles.enableStyle(FileUploadEntry.COMPONENT_NAME);
+        
+        const fileNameElement = this.component.get("fileName");
+        fileNameElement.setChild(this.fileName);
+        
+        const fileSizeElement = this.component.get("fileSize");
+        fileSizeElement.setChild((this.fileSize / 1024).toFixed(2) + " KB");
+        
+        const fileTypeElement = this.component.get("fileType");
+        fileTypeElement.setChild(this.fileType ? this.fileType : "Unknown");
+
+        const removeButton = this.component.get("removeButton");
+        removeButton.listenTo("click", new Method(this, this.removeCliked));
+
+        this.updateProgress(this.file, this.file.name);
+    }
+    
+    /**
+     * 
+     * @param {ContainerEvent} event 
+     */
+    removeCliked(event) {
+        this.events.trigger(FileUploadEntry.EVENT_REMOVE_CLICKED, [event, this.file]);
+    }
+
+    /**
+     * 
+     * @param {ContainerFileData} file 
+     * @param {string} key 
+     */
+    updateProgress(file, key) {
+        if (file) {
+            const progressBar = this.component.get("fileProgressBar");
+            progressBar.setStyle("width", file.uploadPercentage + "%");
+        }
+    }
+}
