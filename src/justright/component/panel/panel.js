ee980cdfbe7a907b2463f7fbe6b0431220324c5a
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2025-10-28 00:52:08 +1100

    Migrate to InlineComponentFactory.

--- src/justright/component/panel/panel.js
+++ src/justright/component/panel/panel.js
@@ -1,13 +1,17 @@
-import { TemplateComponentFactory, Component, CanvasStyles, StyleSelectorAccessor } from "justright_core_v1";
+import { Component,
+    CanvasStyles,
+    StyleSelectorAccessor,
+    StylesheetBuilder,
+    Stylesheet,
+    ComponentBuilder,
+    InlineComponentFactory
+} from "justright_core_v1";
 import { InjectionPoint } from "mindi_v1";
 import { Logger } from "coreutil_v1";
 
 const LOG = new Logger("Panel");
 
 export class Panel {
-
-    static TEMPLATE_URL = "/assets/justrightjs-ui/panel.html";
-    static STYLES_URL = "/assets/justrightjs-ui/panel.css";
 
     static PARAMETER_STYLE_TYPE_COLUMN_ROOT = "panel-type-column-root";
     static PARAMETER_STYLE_TYPE_COLUMN = "panel-type-column";
@@ -39,8 +43,8 @@ export class Panel {
         size = Panel.PARAMETER_STYLE_SIZE_AUTO,
         options = []) {
 
-        /** @type {TemplateComponentFactory} */
-        this.componentFactory = InjectionPoint.instance(TemplateComponentFactory);
+        /** @type {InlineComponentFactory} */
+        this.componentFactory = InjectionPoint.instance(InlineComponentFactory);
 
         /** @type {Component} */
         this.component = null;
@@ -57,6 +61,132 @@ export class Panel {
         /** @type {Array<String>} */
         this.options = options;
 
+    }
+
+    /**
+     * 
+     * @param {StylesheetBuilder} stylesheetBuilder 
+     * @returns {Stylesheet}
+     */
+    static buildStylesheet(stylesheetBuilder) {
+        return stylesheetBuilder
+            .media("@media only screen and (min-width: 850pt)")
+            .open()
+                .selector(".panel-size-responsive")
+                .open()
+                    .style("flex-basis", "100%")
+                    .style("min-width", "800pt")
+                .close()
+            .close()
+
+            .media("@media only screen and (max-width: 849pt)")
+            .open()
+                .selector(".panel-size-responsive")
+                .open()
+                    .style("flex-basis", "100%")
+                    .style("min-width", "500pt")
+                .close()
+            .close()
+
+            .media("@media only screen and (max-width: 500pt)")
+            .open()
+                .selector(".panel-size-responsive")
+                .open()
+                    .style("flex-basis", "100%")
+                    .style("min-width", "100%")
+                .close()
+            .close()
+
+            .selector(".panel-type-column-root")
+            .open()
+                .style("display", "flex")
+                .style("flex-direction", "column")
+                .style("box-sizing", "border-box")
+                .style("height", "100%")
+                .style("border", "0")
+                .style("margin", "0")
+            .close()
+
+            .selector(".panel-type-column")
+            .open()
+                .style("display", "flex")
+                .style("flex-direction", "column")
+                .style("box-sizing", "border-box")
+                .style("margin", "0")
+                .style("border", "0")
+            .close()
+
+            .selector(".panel-type-row")
+            .open()
+                .style("display", "flex")
+                .style("flex-direction", "row")
+                .style("box-sizing", "border-box")
+                .style("margin", "0")
+                .style("border", "0")
+            .close()
+
+            .selector(".panel-content-align-left")
+            .open()
+                .style("justify-content", "left")
+            .close()
+
+            .selector(".panel-content-align-right")
+            .open()
+                .style("justify-content", "right")
+            .close()
+
+            .selector(".panel-content-align-center")
+            .open()
+                .style("align-items", "center")
+                .style("justify-content", "center")
+            .close()
+
+            .selector(".panel-content-align-justify")
+            .open()
+                .style("justify-content", "space-between")
+            .close()
+
+            .selector(".panel-size-auto")
+            .open()
+                .style("flex-grow", "1")
+                .style("flex-shrink", "0")
+                .style("flex-basis", "auto")
+            .close()
+
+            .selector(".panel-size-minimal")
+            .open()
+                .style("flex-grow", "0")
+                .style("flex-shrink", "0")
+                .style("flex-basis", "auto")
+            .close()
+
+            .selector(".panel-content-padding-small")
+            .open()
+                .style("padding", "2pt")
+            .close()
+
+            .selector(".panel-content-padding-large")
+            .open()
+                .style("padding", "6pt")
+            .close()
+
+            .selector(".panel-border-shadow")
+            .open()
+                .style("box-shadow", "0 1px 8px rgba(0,0,0,0.5)")
+            .close()
+
+            .build();
+    }
+
+    /**
+     * 
+     * @param {ComponentBuilder} componentBuilder 
+     * @returns {Component}
+     */
+    static buildComponent(componentBuilder) {
+        return componentBuilder
+            .root("div", "id=panel")
+            .build();
     }
 
     postConfig() {
