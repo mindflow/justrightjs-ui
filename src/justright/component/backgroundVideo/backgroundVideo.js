8351024dad2682d894734395da3fb2d83e3f57f0
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2025-10-27 12:17:03 +1100

    Convert to InlineComponentFactory.

--- src/justright/component/backgroundVideo/backgroundVideo.js
+++ src/justright/component/backgroundVideo/backgroundVideo.js
@@ -1,4 +1,10 @@
-import { VideoElement, CanvasStyles, Component, TemplateComponentFactory } from "justright_core_v1";
+import { VideoElement,
+	CanvasStyles,
+	Component,
+	StylesheetBuilder,
+	ComponentBuilder,
+	InlineComponentFactory 
+ } from "justright_core_v1";
 import { Logger } from "coreutil_v1";
 import { InjectionPoint } from "mindi_v1";
 import { ContainerAsync } from "containerbridge_v1"
@@ -7,19 +13,65 @@ const LOG = new Logger("BackgroundVideo");
 
 export class BackgroundVideo {
 
-	static TEMPLATE_URL = "/assets/justrightjs-ui/backgroundVideo.html";
-	static STYLES_URL = "/assets/justrightjs-ui/backgroundVideo.css";
-
     constructor(videoSrc){
 
-        /** @type {TemplateComponentFactory} */
-        this.componentFactory = InjectionPoint.instance(TemplateComponentFactory);
+        /** @type {InlineComponentFactory} */
+        this.componentFactory = InjectionPoint.instance(InlineComponentFactory);
 
 		/** @type {Component} */
 		this.component = null;
 
         /** @type {String} */
         this.videoSrc = videoSrc;
+	}
+
+	/**
+	 * 
+	 * @param {ComponentBuilder} componentBuilder
+	 * @returns {Component}
+	 */
+	static buildComponent(componentBuilder) {
+		return componentBuilder
+			.root("div", "id=backgroundVideo", "class=background-video").open()
+				.add("div", "class=background-video-overlay")
+				.add("video", "id=video", "class=background-video-player", "playsinline=playsinline",
+						"autoplay=true", "muted=true", "loop=loop").open()
+					.add("source", "id=source", "src=", "type=video/mp4")
+				.close()
+			.close()
+			.build();
+	}
+
+	/**
+	 * 
+     * @param {StylesheetBuilder} stylesheetBuilder
+	 * @returns {String}
+	 */
+	static buildStylesheet(stylesheetBuilder) {
+		return stylesheetBuilder
+			.add(".background-video")
+				.set("width", "auto")
+				.set("height", "auto")
+			.add(".background-video-player")
+				.set("position", "fixed")
+				.set("top", "50%")
+				.set("left", "50%")
+				.set("min-width", "100%")
+				.set("min-height", "100%")
+				.set("width", "auto")
+				.set("height", "auto")
+				.set("transform", "translateX(-50%) translateY(-50%)")
+				.set("z-index", "0")
+			.add(".background-video-overlay")
+				.set("position", "absolute")
+				.set("min-width", "100%")
+				.set("min-height", "100%")
+				.set("width", "auto")
+				.set("height", "auto")
+				.set("background-color", "#1144aa")
+				.set("opacity", "0.3")
+				.set("z-index", "1")
+			.build();
 	}
 
 	set(key,val) {
