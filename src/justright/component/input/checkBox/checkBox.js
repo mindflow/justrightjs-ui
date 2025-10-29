58bbf9d2c229bcb2e2d10d21c43759796febcd09
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2025-10-29 18:31:01 +1100

    Comvert to InlineComponentFactory.

--- src/justright/component/input/checkBox/checkBox.js
+++ src/justright/component/input/checkBox/checkBox.js
@@ -1,8 +1,11 @@
 import {
-    TemplateComponentFactory,
     CanvasStyles,
     Component,
-    InputElementDataBinding
+    InputElementDataBinding,
+    StylesheetBuilder,
+    Stylesheet,
+    ComponentBuilder,
+    InlineComponentFactory
 } from "justright_core_v1";
 import { InjectionPoint } from "mindi_v1";
 import { Logger } from "coreutil_v1";
@@ -11,8 +14,6 @@ const LOG = new Logger("CheckBox");
 
 export class CheckBox {
 
-    static TEMPLATE_URL = "/assets/justrightjs-ui/checkBox.html";
-    static STYLES_URL = "/assets/justrightjs-ui/checkBox.css";
     /**
      * 
      * @param {string} name 
@@ -20,8 +21,8 @@ export class CheckBox {
      */
     constructor(name, model = null) {
         
-        /** @type {TemplateComponentFactory} */
-        this.componentFactory = InjectionPoint.instance(TemplateComponentFactory);
+        /** @type {InlineComponentFactory} */
+        this.componentFactory = InjectionPoint.instance(InlineComponentFactory);
 
         /** @type {Component} */
         this.component = null;
@@ -32,6 +33,100 @@ export class CheckBox {
         /** @type {object} */
         this.model = model;
 
+    }
+
+    /**
+     * 
+     * @param {StylesheetBuilder} stylesheetBuilder 
+     * @returns {Stylesheet}
+     */
+    static buildStylesheet(stylesheetBuilder) {
+        stylesheetBuilder
+            .selector(".check-box")
+            .open()
+                .style("display","block")
+                .style("position","relative")
+                .style("padding-left","2em")
+                .style("margin-bottom","0.5em")
+                .style("cursor","pointer")
+                .style("-webkit-user-select","none")
+                .style("-moz-user-select","none")
+                .style("-ms-user-select","none")
+                .style("user-select","none")
+                .style("margin-bottom","1rem")
+            .close()
+
+            .selector(".check-box input")
+            .open()
+                .style("position","absolute")
+                .style("opacity","0")
+                .style("cursor","pointer")
+                .style("height","0")
+                .style("width","0")
+            .close()
+
+            .selector(".check-box-mark")
+            .open()
+                .style("position","absolute")
+                .style("top","0")
+                .style("left","0")
+                .style("width","calc(1em + 0.5rem + 2px)")
+                .style("height","calc(1em + 0.5rem + 2px)")
+                .style("background-color","#eee")
+            .close()
+
+            .selector(".check-box:hover input ~ .check-box-mark")
+            .open()
+                .style("background-color","#ccc")
+            .close()
+
+            .selector(".check-box input:checked ~ .check-box-mark")
+            .open()
+                .style("background-color","#2196F3")
+            .close()
+
+            .selector(".check-box-mark:after")
+            .open()
+                .style("content","\"\"")
+                .style("position","absolute")
+                .style("display","none")
+            .close()
+
+            .selector(".check-box input:checked ~ .check-box-mark:after")
+            .open()
+                .style("display","block")
+            .close()
+
+            .selector(".check-box .check-box-mark:after")
+            .open()
+                .style("left","0.5em")
+                .style("top","0.4em")
+                .style("width","0.6em")
+                .style("height","0.6em")
+                .style("border","solid white")
+                .style("border-width","0 3px 3px 0")
+                .style("-webkit-transform","rotate(45deg)")
+                .style("-ms-transform","rotate(45deg)")
+                .style("transform","rotate(45deg)")
+            .close();
+
+        return stylesheetBuilder.build();
+    }
+
+    /**
+     * 
+     * @param {ComponentBuilder} componentBuilder 
+     * @returns {Component}
+     */
+    static buildComponent(componentBuilder) {
+       componentBuilder
+            .root("label", "id=check-box", "class=check-box")
+            .open()
+                .node("input", "id=checkBox", "type=checkbox")
+                .node("span", "class=check-box-mark")
+                .text(" Stay logged in")
+            .close();
+        return componentBuilder.build();
     }
 
     postConfig() {
