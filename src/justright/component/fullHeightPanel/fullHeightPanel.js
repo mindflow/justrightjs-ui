778f55dc6a987303260f146975989adfc28d3fb7
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2026-01-09 00:37:20 +1100

    Add fullHeightPanel.

---
+++ src/justright/component/fullHeightPanel/fullHeightPanel.js
@@ -0,0 +1,71 @@
+import { Component,
+    CanvasStyles,
+    StylesheetBuilder,
+    Stylesheet,
+    ComponentBuilder,
+    InlineComponentFactory
+} from "justright_core_v1";
+import { InjectionPoint } from "mindi_v1";
+import { Logger } from "coreutil_v1";
+
+const LOG = new Logger("FullHeightPanel");
+
+export class FullHeightPanel {
+
+
+    /**
+     * 
+     */
+    constructor() {
+
+        /** @type {InlineComponentFactory} */
+        this.componentFactory = InjectionPoint.instance(InlineComponentFactory);
+
+        /** @type {Component} */
+        this.component = null;
+
+    }
+
+    /**
+     * 
+     * @param {StylesheetBuilder} stylesheetBuilder 
+     * @returns {Stylesheet}
+     */
+    static buildStylesheet(stylesheetBuilder) {
+
+        return stylesheetBuilder
+            .selector(".full-height-panel")
+            .open()
+                .style("display", "flex")
+                .style("flex-direction", "column")
+                .style("width", "100%")
+            .close()
+            
+            .selector(".full-height-panel > *")
+            .open()
+                .style("background-color", "#ffffff")
+                .style("flex-grow", "1")
+                .style("flex-shrink", "0")
+                .style("flex-basis", "auto")
+            .close()
+
+            .build();
+    }
+
+    /**
+     * 
+     * @param {ComponentBuilder} componentBuilder 
+     * @returns {Component}
+     */
+    static buildComponent(componentBuilder) {
+        return componentBuilder
+            .root("div", "id=content", "class=full-height-panel")
+            .build();
+    }
+
+    postConfig() {
+        this.component = this.componentFactory.create(FullHeightPanel);
+        CanvasStyles.enableStyle(FullHeightPanel.name);
+    }
+
+}
